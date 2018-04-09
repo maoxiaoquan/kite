34d8e8c85b0e62aedf8aed59f0a2bd2fefa48c64
maoxiaoquan <838115837@qq.com>
2018-04-09 18:32:47 +0800

    fix

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  dev_port: 8081,// 前台开发端口
-  produce_port: 8082,// server端口
+  dev_port: 8060,// 前台开发端口
+  produce_port: 8061,// server端口
   mysql_port: 3306, // 端口号，MySQL默认3306
   database: 'fatecms', // 使用哪个数据库
   username: 'root', // 用户名
