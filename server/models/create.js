56d9c51254e2922e9f8e11bb265157a617a7fed6
maoxiaoquan <838115837@qq.com>
2018-10-18 19:20:34 +0800

    dev ...

--- server/models/create.js
+++ server/models/create.js
@@ -1,16 +1,17 @@
 const models = require('./index')
 /* alter: true */
-/*
+
 models.sequelize.sync({force: true}).then(() => {
   console.log('所有数据表创建完成')
   process.exit()
 })
-*/
+
 
 
 /**/
 
-models.admin_system_log.sync({force: true /* alter: true */}).then(() => {
+/*
+models.admin_system_log.sync({force: true /!* alter: true *!/}).then(() => {
   console.log('所有数据表创建完成')
   process.exit()
-})
+})*/
