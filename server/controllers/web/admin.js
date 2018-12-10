964f59c1f9fa5a46688403e3020c3a696dac5c45
maoxiaoquan <838115837@qq.com>
2018-12-10 22:28:36 +0800

    fix

--- server/controllers/web/admin.js
+++ server/controllers/web/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
+// admin
 module.exports = async (ctx) => {
   const html = fs.readFileSync(path.resolve(__dirname, '../../../static/dist/index.html'), 'utf-8')
   ctx.response.body = html
