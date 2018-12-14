e7a71580bab5c846d1b327de336757e320a23832
maoxiaoquan <838115837@qq.com>
2018-12-14 20:16:58 +0800

    dev ...

--- db/mysqldb/index.js
+++ db/mysqldb/index.js
@@ -10,8 +10,15 @@ const sequelize = new Seq(
   config.database.PASSWORD, // 用户密码
   {
     dialect: 'mysql', // 数据库使用mysql
+    dialectOptions: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      supportBigNumbers: true,
+      bigNumberStrings: true
+    },
     host: config.database.HOST, // 数据库服务器ip
     port: config.database.MYSQL_PORT, // 数据库服务器端口
+    timezone: '+8:00',// 设置东八区
     define: {
       // 字段以下划线（_）来分割（默认是驼峰命名风格）
       underscored: true
