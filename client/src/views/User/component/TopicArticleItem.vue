829a5c042efc6e6bf44e32255d81c18f6b3ac4c6
maoxiaoquan <838115837@qq.com>
2019-07-19 11:45:34 +0800

    fix bug

--- client/src/views/User/component/TopicArticleItem.vue
+++ client/src/views/User/component/TopicArticleItem.vue
@@ -48,7 +48,7 @@
               <el-dropdown-menu slot="dropdown">
                 <el-dropdown-item
                   icon="el-icon-edit"
-                  :command="{name:"editor",params:{editor_type:articleItem.aid}}"
+                  :command="{name:'editor',params:{editor_type:articleItem.aid}}"
                 >修改</el-dropdown-item>
                 <el-dropdown-item icon="el-icon-delete" :command="{name:'delete'}">删除</el-dropdown-item>
               </el-dropdown-menu>
