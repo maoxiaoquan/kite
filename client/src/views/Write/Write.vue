c7be1478bd908f3ad86785b6043845b79b34e50e
maoxiaoquan <838115837@qq.com>
2019-07-31 01:47:30 +0800

    修改编辑器

---
+++ client/src/views/Write/Write.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="write">
+    <client-only>
+      <!-- this component will only be rendered on client-side -->
+      <mavon-editor />
+    </client-only>
+  </div>
+</template>
+
+<script>
+// Local Registration
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import ClientOnly from 'vue-client-only'
+
+export default {
+  name: 'write',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'mavon-editor': mavonEditor,
+    ClientOnly
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.write {
+  margin: auto;
+  width: 100%;
+  height: 580px;
+}
+</style>
+
+
+
