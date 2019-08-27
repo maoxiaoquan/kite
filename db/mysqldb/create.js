3b20d81e544363a4407d5da7e8e96a4f265b460c
maoxiaoquan <838115837@qq.com>
2019-08-27 20:08:32 +0800

    fix

--- db/mysqldb/create.js
+++ db/mysqldb/create.js
@@ -1,12 +1,18 @@
 const models = require('./index')
 
 // 当前js是用来调试 sql 使用，请勿用作其他用途、
-models.sequelize.query(
-  'ALTER TABLE article_tag add COLUMN is_push tinyint(1) DEFAULT 1 comment "是否加入首页或者推荐";'
-)
-models.sequelize.query(
-  'ALTER TABLE dynamic_topic add COLUMN is_push tinyint(1) DEFAULT 1 comment "是否加入首页或者推荐";'
-)
+
+async function sql () {
+  await models.sequelize.query(
+    'ALTER TABLE article_tag add COLUMN is_push tinyint(1) DEFAULT 1 comment "是否加入首页或者推荐";'
+  )
+  await models.sequelize.query(
+    'ALTER TABLE dynamic_topic add COLUMN is_push tinyint(1) DEFAULT 1 comment "是否加入首页或者推荐";'
+  )
+  process.exit()
+}
+sql()
+
 // models.admin_user
 //   .sync({
 //     force: true
